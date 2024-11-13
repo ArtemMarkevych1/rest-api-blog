6f97329a935cdb59270e7c51433b50b3e487448a
amarkevych <artem@govplus.com>
2024-11-13 17:03:04 +0200

    Add CRUD for files

--- routes/file.js
+++ routes/file.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { uploadFile, deleteFile } = require('../controllers/file');
+const { uploadFile, deleteFile, getFiles } = require('../controllers/file');
 const { isAuth, isAdmin } = require('../middlewares');
 const upload = require('../middlewares/upload');
 
+router.get('/', isAuth, getFiles);
 router.post('/upload', isAuth, upload.single('image'), uploadFile);
 router.delete('/:id', isAuth, isAdmin, deleteFile);
 
